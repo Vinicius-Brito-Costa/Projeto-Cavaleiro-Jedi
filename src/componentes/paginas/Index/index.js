2894d2f0f86996301b624bd22d5b73595c7a8b63
Dracods <39202981+Dracods@users.noreply.github.com>
2020-11-30 15:41:35 -0300

    Atualização da pagina inicial

--- src/componentes/paginas/Index/index.js
+++ src/componentes/paginas/Index/index.js
@@ -18,18 +18,19 @@ export default class Produtos extends React.Component{
     }
     render(){
         return(
-            <main className="container-fluid mx-0 px-0 conteudo-site pb-4">
+            <main className="container-fluid mx-0 px-0 conteudo-site">
                 <div className="bg-dark">
-                    <img className="cover1" src={require('../../../imagens/index/aryan-dhiman-iGLLtLINSkw-unsplash.jpg').default} alt="" title="Photo by Aryan Dhiman on Unsplash" />
-                    <div className="comentario">
-                        <div className="texto">
+                    <div className="comentario p-0 m-0">
+                        <div className="my-auto text-light texto">
+                            <div className="my-auto">
                             <h1>Seja bem vindo a Full Stack</h1>
                             <p>Com preços imbatíveis e mais de 10 produtos em seu catálogo, o Full Stack! está sempre à frente e traz em primeira mão os melhores lançamentos do mercado mundial de computadores.</p>
                             <p>O e-commerce é um dos sites mais acessados do país trazendo produtos e atendimento de alta qualidade.</p>
+                            </div>
                         </div>
                     </div>
                     <h1 className="text-white text-center">Produtos em destaque</h1>
-                    <div className="container-fluid w-100 mx-0 pb-2">
+                    <div className="container-fluid w-100 mx-0 pb-4">
                         <div className="container-fluid mx-0 produtos row-cols-2 row-cols-lg-4 px-0 row">
                         {this.state.BD && this.state.BD.map((item, index) => 
                             <Produto
@@ -43,7 +44,6 @@ export default class Produtos extends React.Component{
                                 id = {item['id_categoria']}
                                 imagem = {item['nome_imagem']}
                                 retorno = {item}
-
                             />
 
                         )}
