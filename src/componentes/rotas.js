c0c4ec0062a23e0005f6898916dab8f838608363
Dracods <39202981+Dracods@users.noreply.github.com>
2020-12-07 21:48:21 -0300

    Netlify fix v2

--- src/componentes/rotas.js
+++ src/componentes/rotas.js
@@ -10,10 +10,10 @@ import Produto from './paginas/produto';
 export default function Rotas(){
       return(
             <Switch>
-                <Route exact path="/produtos" component={Produtos}></Route>
-                <Route exact path="/contato" component={Contato}></Route>
-                <Route exact path="/sobre" component={Sobre}></Route>
-				<Route exact path="/produto" component={Produto}></Route>
+                <Route path="/produtos" component={Produtos}></Route>
+                <Route path="/contato" component={Contato}></Route>
+                <Route path="/sobre" component={Sobre}></Route>
+				<Route path="/produto" component={Produto}></Route>
 				<Route exact path="/*" component={Index}></Route>
 				<Route component={Index}></Route>
             </Switch>
